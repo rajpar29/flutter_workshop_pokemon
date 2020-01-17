0cd8ae6f0850a69dd413eafb6332962ec4653832
Hiren <hirenvadher954@gmail.com>
2020-01-17 22:38:34 +0530

    PokeView Update

--- lib/pages/pokeview.dart
+++ lib/pages/pokeview.dart
@@ -6,13 +6,23 @@ import 'package:flutter/material.dart';
 class PokeView extends StatelessWidget {
   Widget _buildList(BuildContext context, DocumentSnapshot document) {
     return Card(
+        color: Colors.cyanAccent,
         child: Column(
-      children: <Widget>[
-        Text(document['name']),
-        Text(document['height']),
-        Text(document['weight']),
-      ],
-    ));
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(document['name']),
+            Text(document['height']),
+            Text(document['weight']),
+            Flexible(
+              fit: FlexFit.tight,
+              child: Image.network(
+                'https://picsum.photos/250?image=9',
+                fit: BoxFit.scaleDown,
+              ),
+            )
+          ],
+        ));
   }
 
   @override
@@ -25,8 +35,10 @@ class PokeView extends StatelessWidget {
           if (!snapshot.hasData) {
             return Text("Loading..");
           }
-          return ListView.builder(
-            itemExtent: 80.0,
+          return GridView.builder(
+            gridDelegate:
+                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+            // itemExtent: 100,
             itemCount: 6,
             itemBuilder: (context, index) {
               return _buildList(context, snapshot.data.documents[index]);
